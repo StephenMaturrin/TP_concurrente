7e65c9f37ccec5c586d8ecf624699007c7b416fe
diegomartingomez14 <diegomartingomez14@gmail.com>
2014-09-28 13:19:48 -0300

    constructor robot

--- TP_Concurrente/src/Robot.java
+++ TP_Concurrente/src/Robot.java
@@ -1,13 +1,51 @@
 
 public class Robot {
 
-	public Robot() {
+	
+	private int id;
+	private int puerto;
+	
+	
+	public Robot(int id,int puerto) {
 		// TODO Auto-generated constructor stub
+	this.id=id;
+	this.puerto=puerto;
 	}
 
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+	public int getPuerto() {
+		return puerto;
+	}
+
+
+
+
+	public void setPuerto(int puerto) {
+		this.puerto = puerto;
 	}
 
 }
