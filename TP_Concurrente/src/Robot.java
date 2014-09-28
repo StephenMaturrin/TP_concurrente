53d221026d4a60f3c0cf7068437bf526bfb0a552
StephenMaturrin <migue.solinas@gmail.com>
2014-09-28 12:50:25 -0300

    Maquina-Robot
    se agrego la clase maquina y robot

---
+++ TP_Concurrente/src/Robot.java
@@ -0,0 +1,13 @@
+
+public class Robot {
+
+	public Robot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
