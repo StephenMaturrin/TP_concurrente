6151eab7f66cb656147c1b5598ffb8ca07e240e7
StephenMaturrin <migue.solinas@gmail.com>
2014-09-28 13:18:36 -0300

    constructor maquina

--- TP_Concurrente/src/Maquina.java
+++ TP_Concurrente/src/Maquina.java
@@ -1,13 +1,32 @@
 
 public class Maquina {
+	
+	private int id;
+	private int puerto;
 
-	public Maquina() {
-		
+	public Maquina(int id,int puerto) {
+		this.id=id;
+		this.puerto=puerto;		
+	}
+	
+	
+	public int getId() {
+		return id;
 	}
 
-	
-	public int uno;
-	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPuerto() {
+		return puerto;
+	}
+
+	public void setPuerto(int puerto) {
+		this.puerto = puerto;
+	}
+
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
